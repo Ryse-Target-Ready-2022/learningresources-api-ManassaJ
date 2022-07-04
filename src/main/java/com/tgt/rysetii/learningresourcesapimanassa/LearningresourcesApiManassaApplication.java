3f6b699caccd7ea391e0baeff34f7296da5b3d9d
Manassa2000 <manassavs2000@gmail.com>
2022-07-04 19:59:17 +0530

    Hello World

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapimanassa/LearningresourcesApiManassaApplication.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapimanassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiManassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiManassaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/learningresourcesapimanassa/LearningresourcesApiManassaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapimanassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiManassaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
